e639b39aadb4c107353552b017d28187fddb8ced
dchang11194 <dchang11194@gmail.com>
2021-04-21 16:59:46 -0400

    setting server up for local FE testing

--- src/main/java/com/example/wbdvsp21myMarathonJourneyserverjava/repositories/UserRepository.java
+++ src/main/java/com/example/wbdvsp21myMarathonJourneyserverjava/repositories/UserRepository.java
@@ -9,15 +9,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long>  {
-  @Query(value = "SELECT * FROM User WHERE userId IN :uid", nativeQuery = true)
+  @Query(value = "SELECT * FROM User WHERE user_id IN :uid", nativeQuery = true)
   public List<User> findListUser(@Param("uid") Long userId);
 
-  @Query(value = "SELECT * FROM User WHERE userId=:uid", nativeQuery = true)
+  @Query(value = "SELECT * FROM User WHERE user_id=:uid", nativeQuery = true)
   public User findUserById(@Param("uid") Long userId);
 
-  @Query(value = "SELECT * FROM Users WHERE userName=:username", nativeQuery = true)
+  @Query(value = "SELECT * FROM Users WHERE user_name=:username", nativeQuery = true)
   public User findUserByUsername(@Param("username") String username);
 
-  @Query(value = "SELECT * FROM Users WHERE userName=:username and password=:password", nativeQuery = true)
+  @Query(value = "SELECT * FROM Users WHERE user_name=:username and password=:password", nativeQuery = true)
   public User findUserByCredentials(@Param("username") String username, @Param("password") String password);
 }
