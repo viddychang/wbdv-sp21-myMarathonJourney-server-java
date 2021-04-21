8d2aecc136a916ad2c2ebb0d4695958d7925a112
dchang11194 <dchang11194@gmail.com>
2021-04-21 17:01:21 -0400

    setting server up for local FE testing

--- src/main/java/com/example/wbdvsp21myMarathonJourneyserverjava/repositories/UserRepository.java
+++ src/main/java/com/example/wbdvsp21myMarathonJourneyserverjava/repositories/UserRepository.java
@@ -15,9 +15,9 @@ public interface UserRepository extends CrudRepository<User, Long>  {
   @Query(value = "SELECT * FROM User WHERE user_id=:uid", nativeQuery = true)
   public User findUserById(@Param("uid") Long userId);
 
-  @Query(value = "SELECT * FROM Users WHERE user_name=:username", nativeQuery = true)
+  @Query(value = "SELECT * FROM User WHERE user_name=:username", nativeQuery = true)
   public User findUserByUsername(@Param("username") String username);
 
-  @Query(value = "SELECT * FROM Users WHERE user_name=:username and password=:password", nativeQuery = true)
+  @Query(value = "SELECT * FROM User WHERE user_name=:username and password=:password", nativeQuery = true)
   public User findUserByCredentials(@Param("username") String username, @Param("password") String password);
 }
